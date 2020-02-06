a2cf3b461f6aa399b552a755d341771aad32dced
Bartek <bartek.stawiarski16@gmail.com>
2020-02-06 22:03:41 +0100

    moved directory and shot_date int its own reducer , created selectors

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,60 +1,20 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component'
 import './directory.styles.scss'
+import {connect} from 'react-redux'
+import {createStructuredSelector} from 'reselect'
+import {selectDirectorySections} from '../../redux/directory/directory.selectors'
 
-class Directory extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            sections: [
-                    {
-                      title: 'CZAPKI',
-                      subtitle: 'KUP TERAZ',
-                      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                      id: 1,
-                      linkUrl: 'shop/hats'
-                    },
-                    {
-                      title: 'KURTKI',
-                      subtitle: 'KUP TERAZ',
-                      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                      id: 2,
-                      linkUrl: 'shop/jackets'
-                    },
-                    {
-                      title: 'BUTY',
-                      subtitle: 'KUP TERAZ',
-                      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                      id: 3,
-                      linkUrl: 'shop/sneakers'
-                    },
-                    {
-                      title: 'DLA NIEJ',
-                      subtitle: 'ZOBACZ KOLEKCJĘ',
-                      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                      size: 'large',
-                      id: 4,
-                      linkUrl: 'shop/womens'
-                    },
-                    {
-                      title: 'DLA NIEGO',
-                      subtitle: 'ZOBACZ KOLEKCJĘ',
-                      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                      size: 'large',
-                      id: 5,
-                      linkUrl: 'shop/mens'
-                    }
-            ]
-        }
-    }
-    render(){
-        return(
-          <div className='directory-menu'>
-          {this.state.sections.map(({ id, ...otherSectionProps }) => (
-            <MenuItem key={id} {...otherSectionProps} />
-          ))}
-        </div>
-        )
-    }
-}
-export default Directory;
+const Directory = ({sections}) => (
+  <div className='directory-menu'>
+  {sections.map(({ id, ...otherSectionProps }) => (
+  <MenuItem key={id} {...otherSectionProps} />
+  ))}
+</div>
+)
+
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
+
+export default connect(mapStateToProps)(Directory);
